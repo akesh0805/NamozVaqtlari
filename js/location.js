31c27d11f555e99d0b957d5751756af66df91a60
akesh0805 <akesh0805@users.noreply.github.com>
2025-01-10 07:22:54 +0000

    deploy: 19d289d14acf7871358a8239585fe0b713eaef43

--- js/location.js
+++ js/location.js
@@ -1,19 +1,34 @@
-window.getUserLocation = function () {  
-    return new Promise((resolve, reject) => {  
-        if (navigator.geolocation) {  
-            navigator.geolocation.getCurrentPosition(  
-                (position) => {  
-                    resolve({  
-                        Latitude: position.coords.latitude,  
-                        Longitude: position.coords.longitude  
-                    });  
-                },  
-                (error) => {  
-                    reject(error);  
-                }  
-            );  
-        } else {  
-            reject(new Error("Geolocation is not supported by this browser."));  
-        }  
-    });  
-};
+function getUserLocation() {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject("Geolocation is not supported by this browser.");
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                },
+                (error) => {
+                    let errorMessage = "";
+                    switch (error.code) {
+                        case error.PERMISSION_DENIED:
+                            errorMessage = "User denied the request for Geolocation.";
+                            break;
+                        case error.POSITION_UNAVAILABLE:
+                            errorMessage = "Location information is unavailable.";
+                            break;
+                        case error.TIMEOUT:
+                            errorMessage = "The request to get user location timed out.";
+                            break;
+                        default:
+                            errorMessage = "An unknown error occurred.";
+                            break;
+                    }
+                    reject(errorMessage);
+                }
+            );
+        }
+    });
+}
